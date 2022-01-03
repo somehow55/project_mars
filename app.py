b18a957d0357767642e3c544e4531b908c3a86f0
somehow55 <somehow55@gmail.com>
2022-01-04 00:59:20 +0900

    2022-01-03, 서버와 클라이언트 POST 연결, mongoDB에 요청해 데이터 넣기

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:1234@cluster0.8s2if.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = client.dbhandey
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route("/mars", methods=["POST"])
 def web_mars_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    name_receive = request.form['name_receive']
+    address_receive = request.form['address_receive']
+    size_receive = request.form['size_receive']
+
+    doc = {
+        'name':name_receive,
+        'address':address_receive,
+        'size':size_receive
+    }
+    db.mars.insert_one(doc);
+
+    return jsonify({'msg': ' 주문 완료!'})
 
 @app.route("/mars", methods=["GET"])
 def web_mars_get():
