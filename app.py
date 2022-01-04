1c425c2a341424d1ce4b7251e3ae1f0e9437fbbb
somehow55 <somehow55@gmail.com>
2022-01-05 00:02:55 +0900

    2022-01-04, POST방식과 GET방식으로 DB데이터 넣고 가져오기

--- app.py
+++ app.py
@@ -10,24 +10,28 @@ db = client.dbhandey
 def home():
     return render_template('index.html')
 
+
 @app.route("/mars", methods=["POST"])
 def web_mars_post():
-    name_receive = request.form['name_receive']
-    address_receive = request.form['address_receive']
-    size_receive = request.form['size_receive']
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
 
     doc = {
-        'name':name_receive,
-        'address':address_receive,
-        'size':size_receive
+        'name': name_receive,
+        'address': address_receive,
+        'size': size_receive
     }
     db.mars.insert_one(doc);
 
-    return jsonify({'msg': ' 주문 완료!'})
+    return jsonify({'msg': '注文 完了!'})
+
 
 @app.route("/mars", methods=["GET"])
 def web_mars_get():
-    return jsonify({'msg': 'msg hereee'})
+    order_list = list(db.mars.find({}, {'_id': False}))
+    return jsonify({'orders': order_list})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
